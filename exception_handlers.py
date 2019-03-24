4b4e83341fdd347cad951bf3f457f5825be78aa2
bashhack (Marc Laughton) <info@marclaughton.com>
2019-03-24 15:12:03 -0400

    Add custom exception handlers

---
+++ exception_handlers.py
@@ -0,0 +1,17 @@
+""" Exception Handlers
+
+- custom exception handlers
+
+"""
+
+
+class RLError(Exception):
+    """ Base class for domain-specific errors
+
+    """
+
+
+class EntityError(RLError):
+    """ Entity error handling
+
+    """
