35bffe90ebb4beb56d39219e919248ad2128083b
bashhack (Marc Laughton) <info@marclaughton.com>
2019-03-25 00:54:24 -0400

    Add game_messages + death_functions for player and monsters

---
+++ death_functions.py
@@ -0,0 +1,41 @@
+""" Death Functions:
+
+- handle death of player and other entities
+
+"""
+
+import tcod
+
+from game_messages import Message
+from game_states import GameStates
+from render_functions import RenderOrder
+
+
+def kill_player(player):
+    """ Update display and kill count on player death
+
+    """
+
+    player.char = "%"
+    player.color = tcod.dark_red
+
+    return Message('You died!', tcod.red), GameStates.PLAYER_DEAD
+
+
+def kill_monster(monster):
+    """ Update display and reset monster attributes on death
+
+    """
+
+    death_message = Message(f"{monster.name.capitalize()} is dead!",
+                            tcod.orange)
+
+    monster.char = "%"
+    monster.color = tcod.dark_red
+    monster.blocks = False
+    monster.fighter = None
+    monster.ai = None
+    monster.name = f"remains of {monster.name}"
+    monster.render_order = RenderOrder.CORPSE
+
+    return death_message
